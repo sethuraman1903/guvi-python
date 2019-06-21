c66e4157e9c1642437ef077c86794425cc3156c8
sethuraman1903 <52067089+sethuraman1903@users.noreply.github.com>
2019-06-21 14:55:08 +0530

    Add files via upload

--- sethu.py
+++ sethu.py
@@ -1,7 +1,7 @@
-a=int(input("enter tha number"))
+a=int(input("Enter the number"))
 if(a>0):
-     print("positive")
+     print("Positive")
 elif(a<0):
-     print("negative")
+     print("Negative")
 else:
-     print("zero")
+     print("Zero")
