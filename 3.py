dc6c4c42a3b5c99b548d46e1c17b16a4f7c93c35
sethuraman1903 <52067089+sethuraman1903@users.noreply.github.com>
2019-06-25 21:11:21 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,9 @@
+l=int(input)
+v=['a','e','i','o','u','A','E','I','O','U']
+c=['q','w','r','t','y','p','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','Q','W','R','T','Y','P','s','D','F','G','H','J','k','L','Z','X','C','V','B','N','M']
+if l in v:
+   print("Vowel")
+elif:
+   print("Consonant")
+else:
+   print("invalid")
