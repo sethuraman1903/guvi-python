21f64611cf805639b18ca9a80d59e2f1830e991a
sethuraman1903 <52067089+sethuraman1903@users.noreply.github.com>
2019-07-03 23:35:00 +0530

    Update 16.py

--- 16.py
+++ 16.py
@@ -1,8 +1,8 @@
 d1,d2=map(int,input().split())
-for i in range(d1,d2):
- if i>1:
+for d3 in range(d1,d2):
+ if d3>1:
   for i in range(2,i):
-   if(i%2)==0:
+   if(d3%i)==0:
     break
    else:
     print(i,end=" ")
