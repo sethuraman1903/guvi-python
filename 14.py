b3f84db31c50ed63991102d26e19470079eab32a
sethuraman1903 <52067089+sethuraman1903@users.noreply.github.com>
2019-07-03 23:21:26 +0530

    Update 14.py

--- 14.py
+++ 14.py
@@ -1,4 +1,4 @@
-start,end=[int(x) for x in input().split()]
-for i in range(start+1,end+1):
+start1,end2=[int(x) for x in input().split()]
+for i in range(start1+1,end2+1):
    if(i%2!=0):
       print(i,end=" ")
